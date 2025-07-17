effbf00b264de93f87b567b58f02f53ead125c76
TotalUmbrella <69628481+TotalUmbrella@users.noreply.github.com>
2025-07-17 19:53:06 +0800

    basic gtk template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("totalumbrella.todolist")
+        .build();
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("simple todo")
+            .default_width(800)
+            .default_height(600)
+            .build();
+
+        window.show();
+    });
+
+    app.run();
 }
